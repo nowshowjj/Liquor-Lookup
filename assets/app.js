9700377f5b7a51fc004bb1c5f622ef1cab0c741d
Amy Schillerstrom <amyschillerstrom@Amys-MacBook-Pro.local>
2018-03-20 19:09:14 -0500

    modal

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,4 @@
-$(document).ready(function(){
-  $('.modal1').modal('open');
-});
-
-$('.modal').modal({
-  dismissible: true, // Modal cannot be dismissed by clicking outside of the modal
-  opacity: .5, // Opacity of modal background
-  inDuration: 300, // Transition in duration
-  outDuration: 200, // Transition out duration
-  startingTop: '4%', // Starting top style attribute
-  endingTop: '10%', // Ending top style attribute
-  ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-    alert("Ready");
-    console.log(modal, trigger);
-  },
-  complete: function() { alert('Closed'); } // Callback for Modal close
-}
-);
-  
+$('#modal1').modal('open');
   
   // Initialize Firebase
   var config = {
