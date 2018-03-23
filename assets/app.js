0adfd2f3e6b5ad753b943dc4347a3ebb75d16acd
J.J. Ayala <jjayala83@gmail.com>
2018-03-23 17:38:27 -0500

    Updated with correct GMaps code.

--- assets/app.js
+++ assets/app.js
@@ -49,47 +49,74 @@
  
   // nav buttons for mobile
   $(".button-collapse").sideNav();
+
+
+  var map;
+  var infowindow;
+  var pos;
+  
+  function initMap() {
+  
+    if (navigator.geolocation) { //GEO LOCATION, FINDS USERS LOCATION
+      navigator.geolocation.getCurrentPosition(function(position) {
+  
+        pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: myLocation,
+          zoom: 12
+        });
+        infoWindow = new google.maps.InfoWindow({
+          map: map
+        });
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        map.setCenter(pos);
+        var myLocation = pos; //Sets variable to geo location long and lat co-ordinates.
+  
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: myLocation, //Uses geolocation to find the following
+          radius: 10000,
+          keyword: ['Liquor+Sales']
+        }, callback);
+      })
+    };
+  
+  
+  
+  }
+  
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+    }
+  }
+  
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+
+      
+
+    });
+  
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+  
+  initMap();
         
 
 
-      // Requires that you consent to location sharing when
-      // prompted by your browser. If you see the error "The Geolocation service
-      // failed.", it means you probably did not give permission for the browser to
-      // locate you.
-      var map, infoWindow;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -30.285126, lng: 97.731556,15},
-          zoom: 8
-        });
-        infoWindow = new google.maps.InfoWindow;
-
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
+  
 
 
